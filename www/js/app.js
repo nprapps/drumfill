2104d0f2ebef1460d17a31615988021f255c6359
Christopher Groskopf <staringmonkey@gmail.com>
2013-04-20 17:41:52 -0400

    Setup router.

--- www/js/app.js
+++ www/js/app.js
@@ -1,51 +1,75 @@
+var $sections;
+var $home_screen;
+var $question_screen;
+var $game_screen;
+var $round_summary_screen;
+var $challenge_friend_screen;
+var $jplayer;
+
 // Index
-var index_page = crossroads.addRoute('', function() {
+crossroads.addRoute('', function() {
+    $home_screen.show();
 });
 
 // Quest
-var quest_page = crossroads.addRoute('quest', function() {
+crossroads.addRoute('quest', function() {
+    $quest_screen.show();
 });
 
 // Turn
-var turn_page = crossroads.addRoute('turn', function() {
-    var $player = $("#pop-audio");
+crossroads.addRoute('game', function() {
+    $game_screen.show();
 
-    $player.jPlayer({
-        ready: function () {
-            $(this).jPlayer("setMedia", {
-                mp3: "audio/20090115_atc_13.mp3"
-            }).jPlayer("play");
-        },
-        swfPath: "js",
-        supplied: "mp3"
-    });
-
-    // associate jPlayer with Popcorn
-    var popcorn = Popcorn('#jp_audio_0');
+    play_audio("audio/20090115_atc_13.mp3");
 });
 
 // Round over
-var round_over_page = crossroads.addRoute('round-over', function() {
+crossroads.addRoute('round-summary', function() {
+    $round_summary_screen.show();
 });
 
 // Challenge
-var challenge_page = crossroads.addRoute('challenge', function() {
+crossroads.addRoute('challenge-friend', function() {
+    $challenge_friend_screen.show();
 });
 
 function parse_hash(new_hash, old_hash) {
     crossroads.parse(new_hash);
 }
 
-function clear_page() {
-    console.log("Clearing the page");
+function clear_screen() {
+    $sections.hide();
+}
+
+function play_audio(filename) {
+    $jplayer.jPlayer("setMedia", {
+        mp3: filename
+    }).jPlayer("play");
 }
 
 crossroads.routed.add(console.log, console);
 crossroads.routed.add(function() {
-    clear_page();
+    clear_screen();
 });
 
 hasher.initialized.add(parse_hash);
 hasher.changed.add(parse_hash);
-hasher.init();
+
+$(function() {
+    $home_screen = $("#home-screen");
+    $game_screen = $("#game-screen");
+    $quest_screen = $("#quest-screen");
+    $round_summary_screen = $("#round-summary-screen");
+    $challenge_friend_screen = $("#challenge-friend-screen");
+
+    $jplayer = $("#pop-audio");
+
+    $jplayer.jPlayer({
+        supplied: "mp3"
+    });
+
+    var popcorn = Popcorn('#jp_audio_0');
+
+    hasher.init();
+});
 
