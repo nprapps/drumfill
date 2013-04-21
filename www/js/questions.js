2d741a5b552ea91e9d10394fb4b87442d3be8be4
Christopher Groskopf <staringmonkey@gmail.com>
2013-04-21 14:58:06 -0400

    Show correct answer on result screen. Closes #54.

--- www/js/questions.js
+++ www/js/questions.js
@@ -1,25 +1,33 @@
 var QUESTIONS = [{
     title: "Witness To Crash Explains What Happened",
+    program: "All Things Considered",
+    date: "January 15, 2009",
     audio: "20090115_atc_13.mp3",
-    question: "What year did this event take place?",
+    question: "What year did this story air?",
     answer: "2009",
     choices: ["2000", "2004", "2009", "2011"]
 },{
     title: "Protesters Defy Curfew As Egypt Deploys Military",
+    program: "All Things Considered",
+    date: "January 28, 2011",
     audio: "20110128_atc_10.mp3",
-    question: "What year did this event take place?",
+    question: "What year did this story air?",
     answer: "2011",
     choices: ["2008", "2009", "2010", "2011"]
 },{
     title: "Rice & Gates Make Case For More Troops In Iraq",
+    program: "All Things Considered",
+    date: "January 11, 2007",
     audio: "20070111_atc_09.mp3",
-    question: "What year did this event take place?",
+    question: "What year did this story air?",
     answer: "2007",
     choices: ["2002", "2003", "2007", "2008"]
 },{
     title: "Overview Of Fannie Mae & Freddie Mac Takeover",
+    program: "All Things Considered",
+    date: "September 11, 2008",
     audio: "20080911_atc_04.mp3",
-    question: "What year did this event take place?",
+    question: "What year did this story air?",
     answer: "2008",
     choices: ["2007", "2008", "2009", "2010"]
 }
