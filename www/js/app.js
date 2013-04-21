ec4143a66b6a86e22f79715e5b42642730c71ca2
Alyson Hurt <ahurt@npr.org>
2013-04-21 13:28:55 -0400

    add button flashes for #49

--- www/js/app.js
+++ www/js/app.js
@@ -125,7 +125,11 @@ function countdown_over($hide_button) {
 
         // Hide the clicked button
         if ($hide_button) {
-            $hide_button.hide();
+            $hide_button.addClass('btn-invalid');
+            $hide_button.animate( { 
+                opacity: .2
+            }, 500);
+            $hide_button.unbind('click');
         // Hide a random button
         } else {
             var $wrong_buttons = [];
